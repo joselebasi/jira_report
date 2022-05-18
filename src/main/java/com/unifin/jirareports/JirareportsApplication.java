dde34e42034d028f87e4927ed1921a5bba936f84
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-05-17 23:00:37 -0500

    [devops] add bean cors

--- src/main/java/com/unifin/jirareports/JirareportsApplication.java
+++ src/main/java/com/unifin/jirareports/JirareportsApplication.java
@@ -27,12 +27,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JirareportsApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JirareportsApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
