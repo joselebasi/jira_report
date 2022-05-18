efe9b73e87c8c9bb71d8f2eea2b63a41dc939a15
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-05-17 19:57:21 -0500

    [devops] change the root path get instead of post

--- src/main/java/com/unifin/jirareports/controller/MainController.java
+++ src/main/java/com/unifin/jirareports/controller/MainController.java
@@ -4,7 +4,7 @@ import com.unifin.jirareports.model.rest.Result;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +13,9 @@ public class MainController {
     @Autowired
     private Environment env;
 
-    @PostMapping("/")
+    @GetMapping("/")
     Result getVersion() {
-        return new Result("Version:1.0"+env.getProperty("MAIL_USERNAME"));
+        return new Result("Version:1.0 +"+env.getProperty("MAIL_USERNAME"));
     }
 
    
