b51f61151e019f4ccd59ddd9e2f7504bf78da432
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-05-19 14:53:21 -0500

    [devops] update env vars 3

--- src/main/java/com/unifin/jirareports/business/jira/BusinessClientJiraServiceImpl.java
+++ src/main/java/com/unifin/jirareports/business/jira/BusinessClientJiraServiceImpl.java
@@ -40,7 +40,7 @@ public class BusinessClientJiraServiceImpl {
     private Environment env;
 
     public void getLsissuesRestTemplate() {
-        String plainCreds = env.getProperty("JIRA_USERNAME")+ ":" +env.getProperty("JIRA_PASWORD");
+        String plainCreds = env.getProperty("JIRA_USERNAME")+ ":" +env.getProperty("JIRA_PASSWORD");
         byte[] plainCredsBytes = plainCreds.getBytes();
         byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
         String base64Creds = new String(base64CredsBytes);
@@ -79,7 +79,7 @@ public class BusinessClientJiraServiceImpl {
             urlSerchJira.append("+and+worklogAuthor=" + worklogAuthor);
         HttpResponse<JsonNode> response = Unirest
                 .get(urlSerchJira.toString())
-                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASWORD")+'"')
+                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASSWORD")+'"')
                 .header("Accept", "application/json")
                 .asJson();
 
@@ -98,7 +98,7 @@ public class BusinessClientJiraServiceImpl {
         String id = issue.optString("id");
         HttpResponse<JsonNode> response = Unirest
                 .get("http://jira.unifin.com.mx:8080/rest/api/2/issue/" + id + "/worklog")
-                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASWORD")+'"')
+                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASSWORD")+'"')
                 .header("Accept", "application/json")
                 .asJson();
         JSONObject body = response.getBody().getObject();
@@ -161,7 +161,7 @@ public class BusinessClientJiraServiceImpl {
 
     public void getUserinfo(String userName) throws Exception {
         HttpResponse<JsonNode> response = Unirest.get("http://jira.unifin.com.mx:8080/rest/api/2/user")
-                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASWORD")+'"')
+                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASSWORD")+'"')
                 .header("Accept", "application/json")
                 .queryString("username ", "jtoledano")
                 .asJson();
@@ -180,7 +180,7 @@ public class BusinessClientJiraServiceImpl {
         // http://unirest.io/java.html
         HttpResponse<JsonNode> response = Unirest
                 .put("http://jira.unifin.com.mx:8080/rest/api/2/user/properties/Consultoria=ACI_group")
-                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASWORD")+'"')
+                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASSWORD")+'"')
                 .header("Accept", "application/json")
                 .header("Content-Type", "application/json")
                 .queryString("accountId", "5b10ac8d82e05b22cc7d4ef5")
@@ -194,10 +194,10 @@ public class BusinessClientJiraServiceImpl {
         StringBuilder urlSerchJira = new StringBuilder(
                 "http://jira.unifin.com.mx:8080/rest/api/2/group/member?groupname=");
         urlSerchJira.append(group.getGroup());
-
+        System.out.println('"'+env.getProperty("JIRA_USERNAME")+'"'+":"+'"'+env.getProperty("JIRA_PASSWORD")+'"');
         HttpResponse<JsonNode> response = Unirest
                 .get(urlSerchJira.toString())
-                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASWORD")+'"')
+                .basicAuth('"'+env.getProperty("JIRA_USERNAME")+'"', '"'+env.getProperty("JIRA_PASSWORD")+'"')
                 .header("Accept", "application/json")
                 .asJson();
         JSONObject body = response.getBody().getObject();
